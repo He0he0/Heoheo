3356a41a1d85a5a8266fe329456299b7cc6d0f1f
He0he0 <105949611+He0he0@users.noreply.github.com>
2025-06-17 22:11:16 -0700

    Added copyright footer. Finished Experience page and styled accordingly

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -8,17 +8,30 @@ import ExperiencePage from "./Experience";
 import ProjectsPage from "./Projects";
 import HomePage from "./Home";
 import NavBar from "./NavBar";
+import "./App.css"
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/experience" element={<ExperiencePage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-      </Routes>
-    </Router>
+    <>
+    <div className="class-container">
+      <div className="content">
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/experience" element={<ExperiencePage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+          </Routes>
+        </Router>
+        <footer className="footer">
+          © {new Date().getFullYear()} Dylan Truong. All rights reserved.
+        </footer>
+     </div>
+    </div>
+
+    </>
+    
   );
 }
 
--- portfolio/src/Experience.jsx
+++ portfolio/src/Experience.jsx
@@ -1,7 +1,101 @@
+import "./Experience.css"
+
+
 const ExperiencePage = () => 
 {
     return (
-        <h1>Experience Page</h1>
+        <div className="content-wrapper"> 
+            <h1>Experience</h1>
+            <p> Below is a list of my professional experience. Feel free to also check out my linkedin for more information.</p>
+            <hr/>
+            <div className="education-wrapper"> 
+                <u><h2>Education</h2></u>
+                <div className="experience-card">
+                    <div className="experience-left">
+                        <b><p>University of California, Los Angeles (UCLA)</p></b>
+                        <p>B.S. Computer Science </p>
+                    </div>
+                    <div className="experience-right">
+                        <i><p>September 2023  - exp. June 2027</p></i>
+                        <p>Los Angeles, California</p>
+                    </div>
+                </div>
+            </div>
+            <p className="coursework-p">
+                <b>Coursework:</b> Software Construction, Data Structures and Algorithms, Intro to Machine Learning, Intro to Computer Architecture, 
+     Circuit Theory
+            </p>
+
+            <hr/>
+            <div className="professional-wrapper"> 
+                <u><h2>Professional Experience</h2></u>
+                <div className="experience-card">
+                    <div className="experience-left">
+                        <b><p>Student Researcher</p></b>
+                        <p>UCLA  Security and Networking (ScaN) Lab </p>
+                    </div>
+                    <div className="experience-right">
+                        <i><p>December 2024  -  June 2025</p></i>
+                    </div>
+                </div>
+                <p>
+                    Contributed to a collaborative research environment by analyzing, modifying, and enhancing an existing codebase for LEO satellite latency measurement using RIPE Atlas data. 
+                    Developed and implemented Python scripts to automate data collection, traceroute parsing, and latency analysis across satellite hops, demonstrating adaptability and teamwork.
+                </p>
+                <div className="experience-card">
+                    <div className="experience-left">
+                        <b><p>Student Researcher</p></b>
+                        <p>Cyber Summer Research Fellowship (CSURF) </p>
+                    </div>
+                    <div className="experience-right">
+                        <i><p>June 2024  - August 2024</p></i>
+                    </div>
+                </div>
+                <p>
+                    Collaborated within a multidisciplinary team to develop a multipart system for dynamic analysis of cryptographic misuses in Node.js applications. 
+                    Conducted in-depth analysis using a modular checker to identify critical vulnerabilities. 
+                    Spearheaded the development of CamNode, a Java-based port of the CamBench benchmark for detecting cryptographic API misuse.
+                </p>
+            </div>
+            <p>
+                
+            </p>
+
+            <hr/>
+            <div className="workexperience-wrapper" >
+                <u><h2>Work Experience</h2></u>
+                <div className="experience-card">
+                    <div className="experience-left">
+                        <b><p>Stockroom Assistant</p></b>
+                        <p>UCLA Samueli School of Engineering</p>
+                    </div>
+                    <div className="experience-right">
+                        <i><p>September 2024  - Present</p></i>
+                        <p>Los Angeles, California</p>
+                    </div>
+                </div>
+                <p>
+                    Provided hands-on training in soldering and electronics repair while demonstrating strong communication and mentorship skills. 
+                    Assisted students with lab project completion and troubleshooting.
+                    Managed and organized lab kits to maintain workflow efficiency and ensure resource availability.
+                </p>
+                <div className="experience-card">
+                    <div className="experience-left">
+                        <b><p>IT Technician</p></b>
+                        <p>ND Auto Smog & Repair</p>
+                    </div>
+                    <div className="experience-right">
+                        <i><p>August 2023  - Present</p></i>
+                        <p>Pomona, California</p>
+                    </div>
+                </div>
+                <p>
+                    Diagnosed and resolved hardware and software issues on legacy computer systems. 
+                    Performed ongoing maintenance, including component replacements and system upgrades.
+                </p>
+            </div>
+            <hr/>
+        </div>
     )
 }
 
